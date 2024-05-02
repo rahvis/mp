5ef68208abacc1530e149fb7de30fd00f40da22f
Rahul Vishwakarma <vishwr7@icloud.com>
2024-05-01 20:31:13 -0700

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,7 +14,7 @@ const About = () => {
     >
       <div className="flex flex-col items-center justify-center">
         <div>
-          <img src={logo} alt={companyName} className="w-16 h-16" />
+          <img src={logo} alt={companyName} className="w-45 h-16" />
         </div>
         <div className="flex flex-wrap sm:gap-10 gap-8 items-center justify-center mt-4 h-12">
           {sections.map((section, index) => (
@@ -28,22 +28,7 @@ const About = () => {
           ))}
         </div>
         <div className="flex items-center gap-x-8 mt-6 h-8">
-          <a
-            aria-label="github"
-            href={socialMedia.github}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <svg
-              className="fill-current text-gray-800 dark:text-white hover:text-primary"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-            </svg>
-          </a>
+         
           <a
             aria-label="linkedin"
             href={socialMedia.linkedin}
@@ -61,8 +46,8 @@ const About = () => {
             </svg>
           </a>
           <a
-            aria-label="twitter"
-            href={socialMedia.twitter}
+            aria-label="instagram"
+            href={socialMedia.instagram}
             target="_blank"
             rel="noreferrer"
           >
@@ -79,9 +64,9 @@ const About = () => {
         </div>
         <div className="flex items-center mt-6">
           <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
-            &copy; {new Date().getFullYear()} designed by{' '}
-            <a href="https://github.com/issaafalkattan" rel="nofollow">
-              Issaaf Kattan
+            &copy; {new Date().getFullYear()} {' '}
+            <a href="" rel="nofollow">
+              Mindful Performance
             </a>
           </p>
         </div>
