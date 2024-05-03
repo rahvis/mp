caaac0136d7e49e44dce72c3fc53ec36688fab41
Rahul Vishwakarma <vishwr7@icloud.com>
2024-05-03 00:20:14 -0700

    commit at 12

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,55 +1,28 @@
 import React from 'react';
 
 import config from '../config/index.json';
+import Divider from './Divider';
 
-const Features = () => {
+const features = () => {
   const { features } = config;
-  const { title, subtitle, description, items: featuresList } = features;
   return (
-    <div className={`py-12 bg-background`} id="features">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
-          <h2
-            className={`text-base text-primary font-semibold tracking-wide uppercase`}
-          >
-            {title}
-          </h2>
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            {subtitle}
-          </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            {description}
-          </p>
-        </div>
-
-        <div className="mt-10">
-          <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-            {featuresList.map((feature) => (
-              <div key={feature.name} className="relative">
-                <dt>
-                  <div
-                    className={`absolute flex items-center justify-center h-12 w-12 rounded-md bg-background text-tertiary border-primary border-4`}
-                  >
-                    <img
-                      className={`inline-block h-6 w-6 rounded-full`}
-                      src={feature.icon}
-                      alt={feature.name}
-                    />
-                  </div>
-                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
-                    {feature.name}
-                  </p>
-                </dt>
-                <dd className="mt-2 ml-16 text-base text-gray-500">
-                  {feature.description}
-                </dd>
-              </div>
-            ))}
-          </dl>
-        </div>
+    <section className={`bg-background py-8`} id="product">
+      <div className={`container max-w-5xl mx-auto m-8`}>
+        <h1
+          className={`w-full my-2 text-5xl font-bold leading-tight text-center text-primary`}
+        >
+          {features.title.split(' ').map((word, index) => (
+            <span
+              key={index}
+              className={index % 2 ? 'text-primary' : 'text-border'}
+            >
+              {word}{' '}
+            </span>
+          ))}
+        </h1>
       </div>
-    </div>
+    </section>
   );
 };
 
-export default Features;
+export default features;
